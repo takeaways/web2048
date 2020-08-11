40a41c72ca99a14e87a57655cdb3d2231059c962
JangGeonil <wkdrjsdlf2@gmail.com>
2020-08-12 01:28:14 +0900

    install lodash

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import Game from './components/Game';
+import AboveGame from './components/AboveGame';
 
 function App() {
   return (
-    <>
+    <div className='container'>
       <Header />
+      <AboveGame />
       <Game />
-    </>
+    </div>
   );
 }
 
